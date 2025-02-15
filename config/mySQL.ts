8cfa18f59e5dba00cf01ca19f1f2ef3c7a3d26f1
unknown <experte001@gmail.com>
2025-02-16 05:19:14 +0900

    Connected Hosted MySQ DataBase🚀

--- config/mySQL.ts
+++ config/mySQL.ts
@@ -12,6 +12,12 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST || "localhost",
     port: Number(process.env.DB_PORT) || 3306,
     logging: process.env.NODE_ENV === "development" ? console.log : false,
+    pool: {
+      max: 5, // Maximum number of connections in pool
+      min: 0, // Minimum number of connections in pool
+      acquire: 30000, // Maximum time (ms) to wait for a connection
+      idle: 10000, // Maximum time (ms) before an unused connection is released
+    },
   }
 );
 
