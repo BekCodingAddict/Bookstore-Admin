f62291ebf7212f67bf4a6489f138139a762de1a3
unknown <experte001@gmail.com>
2025-02-16 09:13:02 +0900

    Fixed : /api/books logic

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -7,9 +7,12 @@ export const GET = async (req: NextRequest) => {
   try {
     const search = req.nextUrl.searchParams.get("search");
 
+    await connectDB();
+
+    let books;
+
     if (!search) {
-      await connectDB();
-      const books = await Book.findAll();
+      books = await Book.findAll();
 
       if (!books || books.length === 0) {
         return new NextResponse(
@@ -17,31 +20,30 @@ export const GET = async (req: NextRequest) => {
           { status: 404, headers: { "Content-Type": "application/json" } }
         );
       }
-      return new NextResponse(JSON.stringify(books), { status: 200 });
+    } else {
+      books = await Book.findAll({
+        where: {
+          [Op.or]: [
+            { author: { [Op.like]: `%${search}%` } },
+            { title: { [Op.like]: `%${search}%` } },
+          ],
+        },
+      });
+
+      if (!books || books.length === 0) {
+        return new NextResponse(
+          JSON.stringify({ books: [], message: "Books not found!" }),
+          {
+            status: 404,
+            headers: { "Content-Type": "application/json" },
+          }
+        );
+      }
     }
 
-    await connectDB();
-    const searchedBooks = await Book.findAll({
-      where: {
-        [Op.or]: [
-          { author: { [Op.like]: `%${search}%` } },
-          { title: { [Op.like]: `%${search}%` } },
-        ],
-      },
-    });
-
-    if (!searchedBooks || searchedBooks.length === 0) {
-      return new NextResponse(
-        JSON.stringify({ books: [], message: "Books not found!" }),
-        {
-          status: 404,
-          headers: { "Content-Type": "application/json" },
-        }
-      );
-    }
-
-    return new NextResponse(JSON.stringify(searchedBooks), { status: 200 });
+    return new NextResponse(JSON.stringify(books), { status: 200 });
   } catch (error) {
+    console.error(error);
     return new NextResponse(
       JSON.stringify({
         message: `Failed to fetch books! Error: ${error}`,
